865b7ae5790da08cccbbb1a81e73298da3bf5073
Icaro Soares <icarosoares01@gmail.com>
2016-06-17 22:05:59 -0300

    Adiciona geração de parametros Adiciona requirement para unicode csv

--- axado.py
+++ axado.py
@@ -1,7 +1,46 @@
 # -*- coding: utf-8 -*-
+import sys
+import os
 
 
 class Axado(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, origem, destino, nota,
+                 peso):
+        """
+        Setando os atributos da classe
+        """
+        self.origem = origem
+        self.destino = destino
+        self.nota = nota
+        self.peso = peso
+        self.path = self.get_path()
+
+    def get_path(self):
+    	"""
+        valida se retorna o path do arquivo
+        """
+        self.path = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_parametros(args=[]):
+    """
+    Reponsável por gerar os parametros passados
+    na chamado do arquivo.
+    """
+    if type(args) is list and len(args) == 5:
+        return {"origem": args[1],
+                "destino": args[2],
+                "nota": args[3],
+                "peso": args[4]}
+    else:
+        return "Erro nos parametros passados"
+
+
+if __name__ == '__main__':
+    params = get_parametros(sys.argv)
+
+    if type(params) is dict:
+        axado = Axado(**params)
+    else:
+        print params
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from axado import Axado
+from axado import Axado, get_parametros
 
 
 class TestAxado(unittest.TestCase):
@@ -13,7 +13,38 @@ class TestAxado(unittest.TestCase):
         """
         # Instanciando a classe Axado para
         # uso durante o processo de test
-        self.axado = Axado()
+        self.axado = Axado('a', 'b', 'c', 'd')
+
+    def test_path(self):
+    	"""
+        Valida se há path
+        """
+        self.assertFalse('', self.axado.path)
+
+    def test_param_error(self):
+        """
+        Valida a geração dos parametros errado passados
+        na chamado do arquivo axado.py
+        """
+        # deixando parametros vazio
+        param = get_parametros()
+        self.assertEquals(param, 'Erro nos parametros passados')
+        # passa parametro de qualquer maneira
+        param = get_parametros('menor exigido')
+        self.assertEquals(param, 'Erro nos parametros passados')
+        # passando lista menor que o exigido
+        param = get_parametros(['a', 'b', 'c'])
+        self.assertEquals(param, 'Erro nos parametros passados')
+
+    def test_param(self):
+        """
+        Valida parametro passado de maneira correta
+        """
+        list_param = ['axado.py', 'florianopolis', 'brasilia', '50', '7']
+        param = get_parametros(list_param)
+        self.assertEquals(type(param), dict)
 
 if __name__ == '__main__':
+
+    # executa os testes
     unittest.main()
